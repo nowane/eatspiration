67db46f7f5672b7edd18dae722b4a533b71402a1
Yorick van den Hurk <nowane@gmail.com>
2021-10-08 14:18:21 +0000

    AdJQuery to show invalid at coconfirm input

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+// Check if confirm password matches password and set valid/invalid
+
+  $("#password-confirm").keyup(function() {
+    if ($( this ).val() === $( "#password" ).val()) {
+      $( this ).removeClass("invalid").addClass("valid");
+      this.setCustomValidity("");
+    } else {
+      $( this ).removeClass("valid").addClass("invalid");
+      this.setCustomValidity("No match");
+    }
+  });
