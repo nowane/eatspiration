e1d7c5f34464d253297c60e548a8388d84bbfd02
Yorick van den Hurk <nowane@gmail.com>
2021-10-11 19:13:22 +0000

    Add jQuery to remove ingredient item

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,38 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav({
-    edge: "right"
-  });
+	$('.sidenav').sidenav({
+		edge: "right"
+	});
 });
 
 // Check if confirm password matches password and set valid/invalid
 $("#password-confirm").keyup(function () {
-  if ($(this).val() === $("#password").val()) {
-    $(this).removeClass("invalid").addClass("valid");
-    this.setCustomValidity("");
-  } else {
-    $(this).removeClass("valid").addClass("invalid");
-    this.setCustomValidity("No match");
-  }
+	if ($(this).val() === $("#password").val()) {
+		$(this).removeClass("invalid").addClass("valid");
+		this.setCustomValidity("");
+	} else {
+		$(this).removeClass("valid").addClass("invalid");
+		this.setCustomValidity("No match");
+	}
 });
 
 // Based on this example
 // https://stackoverflow.com/questions/53400879/how-to-add-new-item-to-materialize-css-collection
-// Add list item to list of ingredients when plus icon is pressed
+// Add list item to list of ingredients when plus icon is clicked
 $('#ingredients .add-ingredient-list-item').click(function (event) {
-  let ingredientListItem = `<li class="collection-item">
+	let ingredientListItem = `<li class="collection-item">
                                 <div class="input-field">
                                     <input name="ingredients" type="text" maxlength="70" required>
-                                    <label for="ingredients">Ingredient</label>
+                                   <label for="ingredients">Ingredient</label>
                                 </div>
                                 <a class="remove-list-item">
-                                <span class="sr-only">Remove Ingredient</span>
-                                    <i class="fas fa-times"></i>
+                                    <span class="sr-only">Remove Ingredient</span>
+                                    <i class="fas fa-trash-alt"></i>
                                 </a>
                             </li>`;
-  $(this).parent().before(ingredientListItem);
+	$(this).parent().before(ingredientListItem);
+});
+
+// Remove ingredient list item on when plus icon is clicked
+$('#ingredients').on("click", ".remove-list-item", function (event) {
+	$(this).parent().remove();
 });
