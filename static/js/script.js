3bfe67175e54dcd1b61451a6728e67c7f5fbdd9f
Yorick van den Hurk <nowane@gmail.com>
2021-10-11 19:55:01 +0000

    Add jQuery function to add to preperation list

--- static/js/script.js
+++ static/js/script.js
@@ -17,12 +17,12 @@ $("#password-confirm").keyup(function () {
 
 // Based on this example
 // https://stackoverflow.com/questions/53400879/how-to-add-new-item-to-materialize-css-collection
-// Add list item to list of ingredients when plus icon is clicked
+// Add ingredient item to list when plus icon is clicked
 $('#ingredients .add-ingredient-list-item').click(function (event) {
 	let ingredientListItem = `<li class="collection-item">
                                 <div class="input-field">
                                     <input name="ingredients" type="text" maxlength="70" required>
-                                   <label for="ingredients">Ingredient</label>
+                                    <label for="ingredients">Ingredient</label>
                                 </div>
                                 <a class="remove-list-item">
                                     <span class="sr-only">Remove Ingredient</span>
@@ -32,7 +32,22 @@ $('#ingredients .add-ingredient-list-item').click(function (event) {
 	$(this).parent().before(ingredientListItem);
 });
 
-// Remove ingredient list item on when plus icon is clicked
+// Remove ingredient list item when thrash icon is clicked
 $('#ingredients').on("click", ".remove-list-item", function (event) {
 	$(this).parent().remove();
+});
+
+// Add preperation item to list when plus icon is clicked
+$('#prep_step .add-prep-list-item').click(function (event) {
+	let prepStep = `<li class="collection-item">
+                        <div class="input-field">
+                            <textarea name="prep_step" class="materialize-textarea" required></textarea>
+                            <label for="prep_step">Step Description</label>
+                        </div>
+						<a class="remove-list-item">
+							<i class="fas fa-trash-alt"></i>
+							<span class="sr-only">Remove Preperation Step</span>
+						</a>
+                    </li>`;
+	$(this).parent().before(prepStep);
 });
