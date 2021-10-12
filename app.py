9df4632e0c2a5efa7a67ca18edce96e556c70426
Yorick van den Hurk <nowane@gmail.com>
2021-10-12 20:46:55 +0000

    Add edit_recipe templste

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def register():
 
         if request.form.get(
             "password") != request.form.get("password-confirm"):
-                flash("Passwords do not match!")
-                return redirect(url_for("register"))
+            flash("Passwords do not match!")
+            return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
@@ -65,11 +65,11 @@ def login():
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 flash("The Username and Password do not match.")
                 return redirect(url_for("login"))
