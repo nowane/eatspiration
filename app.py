633df7d8501712988ac6246682c6dd13eadbf3a0
Yorick van den Hurk <nowane@gmail.com>
2021-10-16 16:31:57 +0000

    Edit function and add helper text in template

--- app.py
+++ app.py
@@ -42,11 +42,10 @@ def register():
             flash("Passwords do not match!")
             return redirect(url_for("register"))
 
-        register = {
+        mongo.db.users.insert_one({
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+        })
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
