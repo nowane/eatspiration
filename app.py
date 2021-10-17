da89ab307fbd7466583538df88b881db1ffa4065
Yorick van den Hurk <nowane@gmail.com>
2021-10-17 15:09:38 +0000

    Add search functionality

--- app.py
+++ app.py
@@ -87,6 +87,14 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    """ Build query function to show the search page and results """
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """ Insert new user into db """
